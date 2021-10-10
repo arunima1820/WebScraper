fbf9cb1116edc1fd491e923f285b75c08067c4f7
Arunima Mittra <arunima1820@gmail.com>
2021-10-10 14:19:27 -0500

    store files works ish

--- Scraper.py
+++ Scraper.py
@@ -1,16 +1,88 @@
+# Manasvini Raghunathan, Arunima Mittra
+# 10/10/21
+# HW5: Web Crawler
+
 import requests
+import bs4
+import re
 from bs4 import BeautifulSoup
 
-URL = 'https://www.cnn.com/2021/10/06/us/zodiac-killer-identity-law-enforcement-investigation/index.html'
 
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-text = soup.get_text()
-print(text)
+def main():
+    URL = 'https://pubmed.ncbi.nlm.nih.gov/12178933/'
+    # returns a list of URLs that are relevant to the chosen URL
 
-for p in soup.select('p'):
-    print(p.get_text())
+    urls = []
+    web_crawler(URL)
+    for url in web_crawler(URL):
+        [urls.append(u) for u in web_crawler(url) if u not in urls]
 
-results = soup.findAll('div', {'zn-body__paragraph'})
-for post in results:
-    print(post.get_text())
+    count = 0
+    dict_url = {}
+    for u in urls:
+        dict_url[count] = u
+        count += 1
+
+
+    # calls function to scrape text off of URLs
+    # puts contents in a separate text file page
+    web_scraper(dict_url)
+
+
+# save files
+def store_file(text, name):
+    with open(str(name) + '.txt', 'w') as f:
+        f.write(text)
+
+
+# clean text
+def clean_text(text):
+    text = re.sub('\n', '', text.strip())
+    text = re.sub('\t', '', text)
+    return text
+
+
+# get list of URLs
+def web_crawler(url):
+    init_page = requests.get(url)
+    soup = BeautifulSoup(init_page.content, 'html.parser')
+    urls = []
+    data = soup.findAll('a', 'docsum-title')
+
+    # first try-catch gathers links in the same domain
+    try:
+        for a in data:
+            regexp = re.compile(r'[0-9]{6,10}')
+            if regexp.search(a.get('href')):
+                urls.append('https://pubmed.ncbi.nlm.nih.gov' + a.get('href'))
+            else:
+                urls.append(a.get('href'))
+    except:
+        print("Exception occured for URL", a)
+
+    # get additional links from within the webpage
+    outside = soup.findAll('ul', 'linkout-category-links')
+    try:
+        for obj in outside:
+            for a in obj.find_all('a'):
+                urls.append(a.get('href'))
+    except:
+        print("Exception occured for URL")
+
+    return urls
+
+
+def web_scraper(dict):
+    for key, val in dict.items():
+        page = requests.get(val)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        results = soup.findAll('div', 'abstract-content selected')
+
+        for post in results:
+            store_file(post.get_text(), str(key))
+            store_file(clean_text(post.get_text()), str(key) + "_clean")
+
+
+if __name__ == '__main__':
+    main()
